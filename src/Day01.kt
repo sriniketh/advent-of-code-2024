3a2dcf1617f933be2f966b038d44456d2701926e
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2024-12-02 23:32:31 -0800

    day 01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,31 @@
+import java.util.PriorityQueue
+import kotlin.math.abs
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val priorityQueue1 = PriorityQueue<Long>()
+        val priorityQueue2 = PriorityQueue<Long>()
+        input.map { line -> line.split(Regex("\\s+")) }
+            .forEach { line ->
+                priorityQueue1.add(line[0].toLong())
+                priorityQueue2.add(line[1].toLong())
+            }
+        var totalDistance = 0L
+        while (priorityQueue1.isNotEmpty() && priorityQueue2.isNotEmpty()) {
+            totalDistance += abs(priorityQueue1.poll() - priorityQueue2.poll())
+        }
+        return totalDistance.toInt()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val lists = input.map { line -> line.split(Regex("\\s+")) }
+            .map { line -> line[0].toLong() to line[1].toLong() }
+            .unzip()
+        // return lists.first.sumOf { number -> lists.second.count { number == it } * number }.toInt()
+        val frequencies = lists.second.groupingBy { it }.eachCount()
+        return lists.first.sumOf { number -> frequencies.getOrDefault(number, 0) * number }.toInt()
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
